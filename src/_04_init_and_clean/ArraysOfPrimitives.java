596114a1475f5f421227e052e309dfd9426da3e8
xuexuan <1099313374@qq.com>
2022-07-08 23:13:08 +0800

    78学习进度

---
+++ src/_04_init_and_clean/ArraysOfPrimitives.java
@@ -0,0 +1,15 @@
+package _04_init_and_clean;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @author xuexuan
+ * @date 2022-07-08 22:35:20
+ */
+public class ArraysOfPrimitives {
+    public static void main(String[] args) {
+        Integer[] a = new Integer[new Random().nextInt(20)];
+        System.out.println(Arrays.toString(a)); // [null, null]
+    }
+}
